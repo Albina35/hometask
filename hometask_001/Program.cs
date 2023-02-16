e350bf271b2ea8a259d41e6891cc933c2cf3305a
Albina <albina_83@mail.ru>
2023-02-16 21:06:01 +0300

    add first task

--- hometask_001/Program.cs
+++ hometask_001/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int A = Convert.ToInt32(Console.ReadLine()); 
+int B = Convert.ToInt32(Console.ReadLine());
+if ( A >  B)
+{
+    Console.WriteLine("min = a");
+}
+else
+{
+    Console.WriteLine("max = b");
+}    
+
