18330cc6e16069f46b9fccde590f07144b95861c
Albina <albina_83@mail.ru>
2023-02-17 22:41:39 +0300

    add task003

--- hometask_001/Program.cs
+++ hometask_001/Program.cs
@@ -1,21 +1,3 @@
-﻿Console.WriteLine("Введите 3 числа:");
-int A = Convert.ToInt32(Console.ReadLine()); 
-int B = Convert.ToInt32(Console.ReadLine());
-int C = Convert.ToInt32(Console.ReadLine());
-
-
-int max = A;
-
-if ( B > max)
-{
-   max = B; 
-}
-
-if (C > max)
-{
-    max = C;
-}    
-
-Console.WriteLine("Большее число", max);
+﻿Console.WriteLine();
 
 
